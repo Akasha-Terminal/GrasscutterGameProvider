72bdf6fd38a4512977c0a450de9f664950dbb1f7
FarlanderCraft <javafarlander2002@gmail.com>
2022-01-29 14:15:30 -0500

    Jitpack just please work you stupid

--- src/main/java/io/github/pseudodistant/provider/patch/MinicraftPatch.java
+++ src/main/java/io/github/pseudodistant/provider/patch/MinicraftPatch.java
@@ -1,18 +1,10 @@
 package io.github.pseudodistant.provider.patch;
 
-import net.fabricmc.loader.impl.game.minecraft.Hooks;
 import net.fabricmc.loader.impl.game.patch.GamePatch;
 import net.fabricmc.loader.impl.launch.FabricLauncher;
-import net.fabricmc.loader.impl.util.log.Log;
-import net.fabricmc.loader.impl.util.log.LogCategory;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
 
-import java.util.ListIterator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
